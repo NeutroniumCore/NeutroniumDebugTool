d88f910d2075662629573f2dd78677471ddf886f
pc-david\david.desmaisons <david.desmaisons@way2.com.br>
2017-02-22 18:23:23 -0300

    semantic screen

---
+++ NeutroniumDebugTool/View/Main3/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="ui grid">
+    <div class="center aligned twelve wide column">
+      <div class="ui main menu">
+      <!--class="ui small basic icon buttons"-->
+          <icon-command :command="DebugBrowser" :class="{'is-active': IsDebuggingVm}" icon="bars" nome="Inspect"></icon-command>
+          <icon-command  v-if="VmDebug" :command="DebugWindow" icon="bug" nome="Vm debugger"></icon-command>
+          <icon-command :command="ShowInfo" icon="info circle" nome="About"></icon-command>
+        </div>
+    </div>
+      
+    <div class="right floated left aligned four wide column">
+    <div class="ui label">
+      <span>Neutronium {{ComponentName}}</span>        
+      <img class="logo" src="./assets/logo.png">
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import iconCommand from './components/icon-command'
+
+const props={
+  viewModel: Object,
+};
+
+export default {
+  components:{
+    iconCommand
+  },
+  name: 'app',
+  props,
+  data () {
+    return this.viewModel
+  }
+}
+</script>
+
+<style>
+@import '~dist/semantic.css';
+
+.logo {
+  height: 50px;
+}
+</style>
---
+++ NeutroniumDebugTool/View/Main3/src/components/icon-command.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :data-tooltip="nome" data-inverted="" data-position="bottom left">
+      <button class="ui button"  @click="execute">
+        <i class="icon" :class="icon" ></i>
+    </button>
+  </div>
+</template>
+
+<script>
+import comandMixin from 'neutronium-vue-command-mixin'
+
+export default {
+  mixins:[comandMixin],
+  props:{
+    nome: String,
+    icon: String
+  }
+}
+</script>
+
+<style>
+</style>
