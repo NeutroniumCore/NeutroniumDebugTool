e4ff3029eec9a933e92a269eb631d418b53f4f0f
desmaisons_david <desmaisons_david@hotmail.com>
2017-03-16 00:06:23 -0300

    Adding new Vm

---
+++ NeutroniumDebugTool/ViewModel/FakeInfoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace NeutroniumDebugTool.ViewModel
+{
+    public class FakeInfoViewModel
+    {
+        public string CoreVersion { get; set; } = "0.4.0";
+        public string WebBrowser { get; set; } = "Chromium 56.0.2924.76";
+        public string BrowserBinding { get; set; } = "ChromiumFX";
+        public string JavascriptFramework { get; set; } = "Vue.js 2.2.1";
+        public string MVVMBinding { get; set; } = "VueInjectorV2";
+    }
+}
